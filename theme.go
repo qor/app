ebdefe3f96777b3ebede7d2ee99b8ca864351867
Jinzhu <wosmvp@gmail.com>
2016-03-24 18:17:22 +0800

    Fix generate templates

--- theme.go
+++ theme.go
@@ -2,12 +2,21 @@ package app
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+var root = "."
+
+func init() {
+	if path := os.Getenv("WEB_ROOT"); path != "" {
+		root = path
+	}
+}
 
 type ThemeInterface interface {
 	GetName() string
@@ -25,25 +34,59 @@ func (theme *Theme) GetName() string {
 	return theme.Name
 }
 
+func isExistingDir(pth string) (string, bool) {
+	if fi, err := os.Stat(pth); err == nil {
+		return pth, fi.Mode().IsDir()
+	}
+	return "", false
+}
+
+func (theme *Theme) GetTemplatesPath() string {
+	if pth, ok := isExistingDir(filepath.Join(root, "vendor", theme.TemplatesPath)); ok {
+		return pth
+	}
+
+	for _, gopath := range strings.Split(os.Getenv("GOPATH"), ":") {
+		if pth, ok := isExistingDir(filepath.Join(gopath, "src", theme.TemplatesPath)); ok {
+			return pth
+		}
+	}
+
+	return theme.TemplatesPath
+}
+
 func (theme *Theme) CopyFiles(app *Application) error {
-	return filepath.Walk(theme.TemplatesPath, func(path string, info os.FileInfo, err error) error {
+	templatesPath := theme.GetTemplatesPath()
+	return filepath.Walk(templatesPath, func(path string, info os.FileInfo, err error) error {
+		fmt.Println(path)
+		fmt.Println(err)
 		if err == nil {
+			var projectPath = theme.Path
+			var relativePath = strings.TrimPrefix(path, templatesPath)
+
+			if projectPath == "" {
+				projectPath = "."
+			}
+
 			if info.IsDir() {
-				err = os.MkdirAll(path, os.ModeDir)
+				err = os.MkdirAll(filepath.Join(projectPath, relativePath), os.ModePerm)
 			} else if info.Mode().IsRegular() {
-				relativePath := strings.TrimPrefix(path, theme.TemplatesPath)
-				if source, err := ioutil.ReadFile(path); err == nil {
+				var source []byte
+				if source, err = ioutil.ReadFile(path); err == nil {
+					fmt.Println(source)
 					if filepath.Ext(path) == ".template" {
-						if tmpl, err := template.New("").Funcs(app.FuncMap()).Parse(string(source)); err == nil {
+						var tmpl *template.Template
+						if tmpl, err = template.New("").Funcs(app.FuncMap()).Parse(string(source)); err == nil {
 							var result = bytes.NewBufferString("")
 							tmpl.Execute(result, app)
 							source = result.Bytes()
 						}
 					}
-					err = ioutil.WriteFile(filepath.Join(theme.Path, strings.TrimSuffix(relativePath, ".template")), source, os.ModePerm)
+					err = ioutil.WriteFile(filepath.Join(projectPath, strings.TrimSuffix(relativePath, ".template")), source, os.ModePerm)
 				}
 			}
 		}
+		fmt.Println(err)
 		return err
 	})
 }
--- web_ec/ec.go
+++ web_ec/ec.go
@@ -2,10 +2,16 @@ package web_ec
 
 import "github.com/qor/app"
 
-type EC struct {
+var EC = &ECTheme{}
+
+func init() {
+	EC.TemplatesPath = "github.com/qor/app/web_ec/templates"
+}
+
+type ECTheme struct {
 	app.Theme
 }
 
-func (ec *EC) ConfigureQorApplication(app *app.Application) {
+func (ec ECTheme) ConfigureQorApplication(app *app.Application) {
 	return
 }
