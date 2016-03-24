0499cba8d2c2da13527e7e8e58ba94f166c305a1
Jinzhu <wosmvp@gmail.com>
2016-03-24 11:59:00 +0800

    Initialize themes, application

--- app.go
+++ app.go
@@ -1,4 +1,20 @@
 package app
 
-func New() {
+type Application struct {
+	Name   string
+	Themes []*ThemeInterface
+}
+
+func New(name string) *Application {
+	return &Application{Name: name}
+}
+
+func (app *Application) Use(theme *ThemeInterface) {
+	app.Themes = append(app.Themes, theme)
+}
+
+func (app *Application) Create() error {
+	for _, theme := range app.Themes {
+	}
+	return
 }
--- theme.go
+++ theme.go
@@ -1,7 +1,33 @@
 package app
 
-type Theme struct {
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type ThemeInterface interface {
+	GetName() string
+	GetPath() string
+	CopyFiles() error
 }
 
-type EC struct {
+type Theme struct {
+	Name          string
+	Path          string
+	TemplatesPath string
+}
+
+func (theme *Theme) CopyFiles() error {
+	return filepath.Walk(theme.TemplatesPath, func(path string, info os.FileInfo, err error) (err error) {
+		if info.IsDir() {
+			err = os.MkdirAll(path, os.O_RDWR)
+		} else if info.Mode().IsRegular() {
+			relativePath := strings.TrimPrefix(path, template.TemplatesPath)
+			if source, err := io.ReadFile(path); err == nil {
+				err = io.WriteFile(filepath.Join(theme.Path, relativePath), source, os.O_RDWR)
+			}
+		}
+		return
+	})
 }
