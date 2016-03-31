c6a226aa5b36b824c258d3feaf84a8033f3d481a
Jinzhu <wosmvp@gmail.com>
2016-03-31 16:54:59 +0800

    Update home template

--- modules/products/product.go
+++ modules/products/product.go
@@ -5,3 +5,9 @@ import "github.com/qor/app"
 type Product struct {
 	app.Plugin
 }
+
+// config/admin/admin.go
+// 	Admin.AddResource(&models.Product{})
+
+// config/routers/routes.go
+//   insert route
