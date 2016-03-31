874d63571f5a5b02ca803db0af4e48b2d3d374c4
Jinzhu <wosmvp@gmail.com>
2016-03-31 11:58:45 +0800

    Improve generated app structure

--- web_ec/templates/config/config.go
+++ web_ec/templates/config/config.go
@@ -1,15 +1,26 @@
 package config
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/jinzhu/configor"
 	"github.com/qor/render"
 )
 
 var Render *render.Render
-var DB *gorm.DB
+var Config = struct {
+	Port uint `default:"7000" env:"PORT"`
+	DB   struct {
+		Name     string
+		Adapter  string `default:"sqlite"`
+		User     string
+		Password string
+		Host     string `default:"localhost"`
+	}
+}{}
 
 func init() {
+	if err := configor.Load(&Config, "config/database.yml", "config/application.yml"); err != nil {
+		panic(err)
+	}
+
 	Render = render.New()
-	DB, _ = gorm.Open("sqlite3", "test.db")
 }
